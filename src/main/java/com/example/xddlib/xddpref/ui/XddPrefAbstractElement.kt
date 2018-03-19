722fe79aae340c11c249068ef4ad5ae571da94d2
Owen_Chen <owen_chen@asus.com>
2018-03-19 21:22:26 +0800

    [Refactor][Kotlin] XddPrefAbstractElement, XddPrefToggleElement, XddPrefRadioElement

---
+++ src/main/java/com/example/xddlib/xddpref/ui/XddPrefAbstractElement.kt
@@ -0,0 +1,55 @@
+package com.example.xddlib.xddpref.ui
+
+import android.content.Context
+import android.support.annotation.ColorInt
+import android.util.AttributeSet
+import android.widget.Button
+import android.widget.HorizontalScrollView
+import com.example.xddlib.R
+import com.example.xddlib.XddInternalUtils
+import com.example.xddlib.xddpref.data.XddPrefAbstractData
+
+/**
+ * Created by adher on 2017/7/20.
+ */
+
+internal abstract class XddPrefAbstractElement(context: Context, attributeSet: AttributeSet?) : HorizontalScrollView(context, attributeSet) {
+
+    lateinit var mPrefData: XddPrefAbstractData<*>
+    private lateinit var mResetButton: Button
+
+    internal abstract val uiValue: Any
+
+    init {
+        if (sColorPrimary == -1) {
+            sColorPrimary = XddInternalUtils.getColorAttr(context, android.R.attr.colorAccent)
+        }
+    }
+
+    internal open fun init(prefData: XddPrefAbstractData<*>) {
+        mPrefData = prefData
+
+        mResetButton = findViewById(R.id.key)
+        mResetButton.text = prefData.key
+        mResetButton.setOnClickListener { resetToSharedValue() }
+    }
+
+    /**@return true if UiValue == SharedValue
+     */
+    fun onUiValueChanged(uiValue: Any): Boolean {
+        val equalToShared = mPrefData.sharedValueIsEqualTo(uiValue)
+        mResetButton.isEnabled = !equalToShared
+        return equalToShared
+    }
+
+    internal abstract fun resetToSharedValue()
+
+    fun saveToNative() {
+        mPrefData.saveToNativePreference(uiValue)
+    }
+
+    companion object {
+        @ColorInt
+        var sColorPrimary = -1
+    }
+}
---
+++ src/main/java/com/example/xddlib/xddpref/ui/XddPrefRadioElement.kt
@@ -0,0 +1,64 @@
+package com.example.xddlib.xddpref.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import com.example.xddlib.R
+import com.example.xddlib.XddInternalUtils
+import com.example.xddlib.xddpref.data.XddPrefAbstractData
+import com.example.xddlib.xddpref.data.XddPrefEnumData
+import junit.framework.Assert
+
+/**
+ * Created by adher on 2017/7/21.
+ */
+
+internal class XddPrefRadioElement(context: Context, attributeSet: AttributeSet?) : XddPrefAbstractElement(context, attributeSet) {
+    private lateinit var mRadioGroup: RadioGroup
+
+    override val uiValue: Any
+        get() = (findViewById<View>(mRadioGroup.checkedRadioButtonId) as RadioButton).text
+
+    override fun init(prefData: XddPrefAbstractData<*>) {
+        Assert.assertTrue(prefData is XddPrefEnumData)
+        super.init(prefData)
+
+        val enumData = prefData as XddPrefEnumData<*>
+
+        mRadioGroup = findViewById(R.id.radioGroup)
+        mRadioGroup.removeAllViews()
+        mRadioGroup.setOnCheckedChangeListener { group, checkedId -> onUiValueChanged((group.findViewById<View>(checkedId) as RadioButton).text) }
+
+        //create radio buttons based on values in pref
+        val prefValueAsString = mPrefData[false].toString()
+        for (oneEnum in enumData.values) {
+            val radioButton = RadioButton(this.context)
+            mRadioGroup.addView(radioButton) //the RadioButtons are added to the radioGroup instead of the layout
+            radioButton.text = oneEnum.toString()
+
+            if (prefValueAsString == radioButton.text) {
+                radioButton.isChecked = true
+                radioButton.setTextColor(XddPrefAbstractElement.Companion.sColorPrimary)
+            }
+        }
+    }
+
+    override fun resetToSharedValue() {
+        for (i in 0 until mRadioGroup.childCount) {
+            val button = mRadioGroup.getChildAt(i) as RadioButton
+            if (mPrefData.sharedValueIsEqualTo(button.text)) {
+                button.isChecked = true
+                break
+            }
+        }
+    }
+
+    companion object {
+
+        fun inflate(parent: XddPrefContainer): XddPrefRadioElement {
+            return XddInternalUtils.inflate(XddPrefRadioElement::class.java, R.layout.xdd_pref_dialog_radio_element_constructor, parent)
+        }
+    }
+}
