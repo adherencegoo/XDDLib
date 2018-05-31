5355509491c957947e0deeb7b4e0eef2c282c907
Owen_Chen <owen_chen@asus.com>
2018-05-31 14:10:25 +0800

    [Behavior][checkDatabaseEquality]  - throw Exception if table specs are not equal or sql exception  - return different ContentValues

--- src/main/java/com/example/xddlib/XDD.kt
+++ src/main/java/com/example/xddlib/XDD.kt
@@ -9,10 +9,8 @@ import android.database.sqlite.SQLiteException
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
-import android.media.MediaScannerConnection
 import android.net.Uri
 import android.os.Build
-import android.os.Environment
 import android.os.Looper
 import android.os.Vibrator
 import android.provider.MediaStore
@@ -20,13 +18,11 @@ import android.support.annotation.ColorInt
 import android.text.TextUtils
 import com.example.xddlib.analysis.XddException
 import com.example.xddlib.presentation.Lg
-import com.example.xddlib.presentation.XddToast
 import com.example.xddlib.userinput.xddpref.data.NativePreferenceHelper
 import junit.framework.Assert
 import java.io.File
 import java.io.FileNotFoundException
-import java.io.FileOutputStream
-import java.io.OutputStream
+import java.security.InvalidParameterException
 import java.util.*
 
 /** Created by Owen_Chen on 2017/3/15.  */
@@ -157,15 +153,38 @@ object XDD {
         (context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(ms)
     }
 
-    /**@return root cause of not equal*/
     @JvmStatic
-    fun checkDatabaseEquality(fullPath1: String, fullPath2: String, checkByteArray: Boolean = true, vararg tables: String): Throwable? {
-        Assert.assertTrue(tables.isNotEmpty())
-        if (!File(fullPath1).exists()) {
-            return FileNotFoundException(fullPath1).also { Lg.e(it) }
+    fun dumpCheckDatabaseQualityResult(differences: Map<String, Map<Int, Pair<ContentValues, ContentValues>>>): String {
+        return differences.entries.joinToString(separator = ";\n") {
+            "\ntable:${it.key} --> \n" + it.value.entries.joinToString(separator = ";\n") {
+                "\trowIndex:${it.key} --> \n" +
+                        "\t\tvalue1: ${it.value.first}\n" +
+                        "\t\tvalue2: ${it.value.second}"
+            }
         }
-        if (!File(fullPath2).exists()) {
-            return FileNotFoundException(fullPath2).also { Lg.e(it) }
+    }
+
+    @JvmStatic
+    fun checkDatabaseEquality(fullPath1: String,
+                              fullPath2: String,
+                              tables: List<String>)
+            : Map<String, Map<Int, Pair<ContentValues, ContentValues>>> {
+        return checkDatabaseEquality(fullPath1, fullPath2, tables.map { it to null }.toMap())
+    }
+
+    /**@param tableDetailList key: tableName, value: interesting column names (take all columns if null)
+     * @return key: tableName, value: pairs of different [ContentValues]s indexed with rowIndex*/
+    @Suppress("MemberVisibilityCanBePrivate")
+    @JvmStatic
+    @Throws(SQLiteException::class, XddException::class)
+    fun checkDatabaseEquality(fullPath1: String,
+                              fullPath2: String,
+                              tableDetailList: Map<String, Collection<String>?>)
+            : Map<String/*tableName*/, Map<Int/*rowIndex*/, Pair<ContentValues, ContentValues>>> {
+        if (tableDetailList.isEmpty()) throw XddException(InvalidParameterException("empty tables").message)
+
+        listOf(fullPath1, fullPath2).forEach {
+            if (!File(it).exists()) throw XddException(FileNotFoundException(it).message)
         }
 
         val tagDb = Lg.getPrioritizedMessage(Lg.DEFAULT_INTERNAL_LG_TYPE)
@@ -173,9 +192,10 @@ object XDD {
                 Lg.LF + Lg.TAB, fullPath1,
                 Lg.LF + Lg.TAB, fullPath2)
 
+        val differences = mutableMapOf<String, MutableMap<Int, Pair<ContentValues, ContentValues>>>()
         if (fullPath1 == fullPath2) {
             Lg.w(tagDb, "*** same db file***")
-            return null
+            return differences
         }
 
         var db1: SQLiteDatabase? = null
@@ -184,78 +204,102 @@ object XDD {
             db1 = SQLiteDatabase.openDatabase(fullPath1, null, SQLiteDatabase.OPEN_READONLY)
             db2 = SQLiteDatabase.openDatabase(fullPath2, null, SQLiteDatabase.OPEN_READONLY)
 
-            tables.forEach { table ->
-                val tagTable = Lg.getPrioritizedMessage(tagDb, "table:$table")
+            tableDetailList.forEach { tableDetail ->
+                val tagTable = Lg.getPrioritizedMessage(tagDb, "tableDetail:$tableDetail")
                 Lg.log(tagTable, "Comparing ...")
 
                 var cursor1: Cursor? = null
                 var cursor2: Cursor? = null
                 try {
-                    cursor1 = db1.query(table, null, null, null, null, null, null)
-                    cursor2 = db2.query(table, null, null, null, null, null, null)
+                    val columns = tableDetail.value?.toTypedArray() // Nullable
+                    cursor1 = db1.query(tableDetail.key, columns, null, null, null, null, null)
+                    cursor2 = db2.query(tableDetail.key, columns, null, null, null, null, null)
 
                     if (cursor1 == null || cursor2 == null) {
-                        return XddException(tagTable, "cursor1:$cursor1", "cursor2:$cursor2")
-                                .also { Lg.e(it) }
+                        throw XddException(tagTable, "null cursor",
+                                "cursor1:$cursor1", "cursor2:$cursor2")
+
                     } else if (cursor1.columnCount != cursor2.columnCount) {// Column count
-                        return XddException(tagTable, "Column counts mismatch!",
+                        throw XddException(tagTable, "Column counts mismatch",
                                 Lg.LF + Lg.TAB, cursor1.columnNames,
                                 Lg.LF + Lg.TAB, cursor2.columnNames)
-                                .also { Lg.e(it) }
+
                     } else if (!(cursor1.columnNames contentDeepEquals cursor2.columnNames)) {
-                        return XddException(tagTable, "Column names mismatches",
+                        throw XddException(tagTable, "Column names mismatch",
                                 Lg.LF + Lg.TAB, cursor1.columnNames,
                                 Lg.LF + Lg.TAB, cursor2.columnNames)
-                                .also { Lg.e(it) }
+
                     } else if (cursor1.count != cursor2.count) {// Row count
-                        return XddException(tagTable, "Row counts mismatch!",
+                        throw XddException(tagTable, "Row counts mismatch",
                                 "${cursor1.count} v.s. ${cursor2.count}")
-                                .also { Lg.e(it) }
+
                     } else {// Row content
+                        var checkColType = true
+                        val columnIndices = IntArray(cursor1.columnCount) { it }
+
                         val values1 = ContentValues()
                         val values2 = ContentValues()
                         while (cursor1.moveToNext() and cursor2.moveToNext()) {// For each row
-                            val tagRow = Lg.getPrioritizedMessage(tagTable,
-                                    "Row contents not equal!",
-                                    "rowIndex:${cursor1.position}")
+                            if (checkColType) {
+                                checkColType = !checkColType
+                                // Cursor.getType must be invoked after moveToFist
+                                if (columnIndices.map { cursor1.getType(it) == cursor2.getType(it) }
+                                                .contains(false)) {// Column type
+                                    throw XddException(tagTable, "Column types mismatch",
+                                            Lg.LF + Lg.TAB, "cursor1 column type:", columnIndices.map(cursor1::getType),
+                                            Lg.LF + Lg.TAB, "cursor2 column type:", columnIndices.map(cursor2::getType))
+                                }
+                            }
 
                             DatabaseUtils.cursorRowToContentValues(cursor1, values1)
                             DatabaseUtils.cursorRowToContentValues(cursor2, values2)
 
+                            var diffValue1: ContentValues? = null
+                            var diffValue2: ContentValues? = null
+
                             /* Extract blob(byte array) form Cursor and compare them,
                             Remove blob from ContentValues*/
-                            for (colIdx in 0 until cursor1.columnCount) {
-                                if (cursor1.getType(colIdx) == Cursor.FIELD_TYPE_BLOB
-                                        && cursor2.getType(colIdx) == Cursor.FIELD_TYPE_BLOB) {
-                                    val colName = cursor1.getColumnName(colIdx)
-                                    // If need not check byte array, directly remove Blob without comparison
-                                    if (!checkByteArray || cursor1.getBlob(colIdx) contentEquals cursor2.getBlob(colIdx)) {
-                                        values1.remove(colName)
-                                        values2.remove(colName)
-                                    } else {
-                                        return XddException(tagRow, "byteArray! colName:$colName")
-                                                .also { Lg.e(it) }
+                            for (indexedName in cursor1.columnNames.withIndex()) {
+                                if (cursor1.getType(indexedName.index) == Cursor.FIELD_TYPE_BLOB) {
+                                    values1.remove(indexedName.value)
+                                    values2.remove(indexedName.value)
+
+                                    val blob1 = cursor1.getBlob(indexedName.index)
+                                    val blob2 = cursor2.getBlob(indexedName.index)
+                                    if (!(blob1 contentEquals blob2)) {
+                                        if (diffValue1 == null) diffValue1 = ContentValues()
+                                        if (diffValue2 == null) diffValue2 = ContentValues()
+                                        diffValue1.put(indexedName.value, blob1)
+                                        diffValue2.put(indexedName.value, blob2)
                                     }
                                 }
                             }
 
                             if (values1 != values2) {// Doesn't handle byte array equality
-                                return XddException(tagRow,
-                                        Lg.LF + Lg.TAB + "[values1]:", values1,
-                                        Lg.LF + Lg.TAB + "[values2]:", values2)
-                                        .also { Lg.e(it) }
+                                if (diffValue1 == null) diffValue1 = ContentValues()
+                                if (diffValue2 == null) diffValue2 = ContentValues()
+
+                                // Only show different value
+                                cursor1.columnNames.forEach {
+                                    val string1 = values1.getAsString(it)
+                                    val string2 = values2.getAsString(it)
+                                    if (string1 != string2) {
+                                        diffValue1.put(it, string1)
+                                        diffValue2.put(it, string2)
+                                    }
+                                }
+                            }
+
+                            if (diffValue1 != null && diffValue2 != null) {
+                                differences.getOrPut(tableDetail.key) { mutableMapOf() }[cursor1.position] = Pair(diffValue1, diffValue2)
                             }
                         }
                     }
-                } catch (e: SQLiteException) {
-                    return XddException(tagDb, e.message).also { Lg.e(it) }
                 } finally {
                     cursor1?.close()
                     cursor2?.close()
                 }
             }
-        } catch (e: SQLiteException) {
-            return XddException(tagDb, e.message).also { Lg.e(it) }
         } finally {
             db1?.close()
             db2?.close()
@@ -263,6 +307,6 @@ object XDD {
             Lg.log(tagDb, "All done")
         }
 
-        return null
+        return differences
     }
 }
